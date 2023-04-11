71424e507659708518a8bda5ac0aa942542b5a54
AnnaZernina <110138099+AnnaZernina@users.noreply.github.com>
2023-04-11 14:15:34 +0300

    new commit

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
-public class main {
+public class Main {
 
     public static void main(String[] args) {
 
         int price = 100; //стоимость билета
         int miles = price / 20; // кол-во руб для одной б/мили
-        system.out.println("Количество бонусных миль: " + miles);// ответ
+        System.out.println("Количество бонусных миль: " + miles);// ответ
 
     }
 }

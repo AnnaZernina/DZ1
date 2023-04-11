86fbf18a563f5d3ace52f04ba1b38365236daaff
AnnaZernina <110138099+AnnaZernina@users.noreply.github.com>
2023-04-11 11:02:04 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
-public class Main {
+public class main {
 
     public static void main(String[] args) {
 
         int price = 100; //стоимость билета
         int miles = price / 20; // кол-во руб для одной б/мили
-        System.out.println("Количество бонусных миль: " + miles);// ответ
+        system.out.println("Количество бонусных миль: " + miles);// ответ
 
     }
 }
